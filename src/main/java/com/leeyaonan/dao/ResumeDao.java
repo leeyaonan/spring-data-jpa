1b3b9d5fddf940a47fed8e2fe2f555e3598aad2f
leeyaonan <leeyaonan@outlook.com>
2020-05-05 11:22:52 +0800

    Dao层接口规范

---
+++ src/main/java/com/leeyaonan/dao/ResumeDao.java
@@ -0,0 +1,20 @@
+package com.leeyaonan.dao;
+
+import com.sun.xml.bind.v2.model.core.ID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * 一个符合SpringDataJPA要求的Dao层接口是需要继承JpaRepository和JpaSpecificationExecutor
+ *
+ * JpaRepository<操作的实体类型，主键类型>
+ *     封装了基本的CRUD操作
+ *
+ * JpaSpecificationExecutor<操作的实体类类型>
+ *     封装了复杂的查询（分页、排序等）
+ * @Author leeyaonan
+ * @Date 2020/5/5 11:18
+ */
+public interface ResumeDao extends JpaRepository<T, ID>, JpaSpecificationExecutor {
+
+}
